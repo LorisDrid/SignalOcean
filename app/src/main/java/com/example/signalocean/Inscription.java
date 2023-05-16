4b69ffdaf27a83f87dda7166aafbeeeff52ec173
AmirBourry <amir.bourry@etu.univ-cotedazur.fr>
2023-05-16 15:24:18 +0200

    Ajout des routing manquants

--- app/src/main/java/com/example/signalocean/Inscription.java
+++ app/src/main/java/com/example/signalocean/Inscription.java
@@ -12,5 +12,23 @@ public class Inscription extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inscription);
+
+        Button retour = (Button) findViewById(R.id.retour);
+        retour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Inscription.this, Connexion.class);
+                startActivity(intent);
+            }
+        });
+
+        Button connexion = (Button) findViewById(R.id.connexion);
+        connexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Inscription.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
