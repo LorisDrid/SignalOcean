8dd7846e8d9702626c0d25149ec441a1302873c2
RomainTho <romain.tho@etu.univ-cotedazur.fr>
2023-06-07 18:29:12 +0200

    essaie caméra

--- app/src/main/java/com/example/signalocean/CreatePostActivity.java
+++ app/src/main/java/com/example/signalocean/CreatePostActivity.java
@@ -1,38 +1,47 @@
 package com.example.signalocean;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 
 import java.util.Optional;
-
 public class CreatePostActivity extends AppCompatActivity {
 
-    private static final int SELECT_IMAGE_REQUEST_CODE = 1 ;
+    private static final int SELECT_IMAGE_REQUEST_CODE = 1;
+    private static final int CAMERA_REQUEST_CODE = 2;
+    private static final int REQUEST_CAMERA_PERMISSION = 3;
+    private static final int REQUEST_STORAGE_PERMISSION = 4;
+
     private EditText editTitle;
     private EditText editText;
     private Button btnCreatePost;
+    private Button btnReturn;
+    private MapView mapView;
+    private Button btnInsererImage;
     private String type;
     private Optional<Drawable> image;
     private Uri imageUri;
     private GeoPoint location;
 
-    private Button btnReturn;
-    private MapView mapView;
-    private Button btnInsererImage;
     private AbstractPostFactory abstractPostFactory;
 
     @Override
@@ -50,18 +59,15 @@ public class CreatePostActivity extends AppCompatActivity {
         btnCreatePost = findViewById(R.id.btnCreatePost);
         btnReturn = findViewById(R.id.retour);
 
-
         btnInsererImage = findViewById(R.id.insérer_image);
 
         btnInsererImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Open a dialog or activity to select an image from the storage
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
-                startActivityForResult(intent, SELECT_IMAGE_REQUEST_CODE);
+                showImageSourceSelectionDialog();
             }
         });
+
         editText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -92,22 +98,78 @@ public class CreatePostActivity extends AppCompatActivity {
                 String title = editTitle.getText().toString();
                 String text = editText.getText().toString();
 
-
-                AbstractPost post = abstractPostFactory.createPost(type ,title, text, Optional.ofNullable(CreatePostActivity.this.imageUri),location);
+                AbstractPost post = abstractPostFactory.createPost(type, title, text, Optional.ofNullable(CreatePostActivity.this.imageUri), location);
                 MainActivity.getCurrentUser().getPosts().add(post);
 
                 Toast.makeText(CreatePostActivity.this, "type post = " + post.getType(), Toast.LENGTH_SHORT).show();
-
 
                 Toast.makeText(CreatePostActivity.this, "Post créé avec succès", Toast.LENGTH_SHORT).show();
                 int postCount = MainActivity.getCurrentUser().getPosts().size();
                 Toast.makeText(CreatePostActivity.this, "Nombre de posts : " + postCount, Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(CreatePostActivity.this, Maps.class);
                 startActivity(intent);
-
             }
         });
     }
+
+    private void showImageSourceSelectionDialog() {
+        String[] options = {"Galerie", "Caméra"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Sélectionner une image depuis :");
+        builder.setItems(options, (dialog, which) -> {
+            switch (which) {
+                case 0:
+                    // Gallery option selected
+                    if (checkStoragePermission()) {
+                        openGallery();
+                    } else {
+                        requestStoragePermission();
+                    }
+                    break;
+                case 1:
+                    // Camera option selected
+                    if (checkCameraPermission()) {
+                        openCamera();
+                    } else {
+                        requestCameraPermission();
+                    }
+                    break;
+            }
+        });
+        builder.show();
+    }
+
+    private boolean checkStoragePermission() {
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean checkCameraPermission() {
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int result2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED && result2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_STORAGE_PERMISSION);
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CAMERA_PERMISSION);
+    }
+
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, SELECT_IMAGE_REQUEST_CODE);
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -119,7 +181,38 @@ public class CreatePostActivity extends AppCompatActivity {
             // Display the image in the ImageView
             ImageView imageView = findViewById(R.id.imageView);
             imageView.setImageURI(this.imageUri);
+        } else if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            // Retrieve the captured image
+            Bundle extras = data.getExtras();
+            if (extras != null && extras.containsKey("data")) {
+                // Get the image bitmap
+                Drawable imageDrawable = (Drawable) extras.get("data");
+
+                // Display the image in the ImageView
+                ImageView imageView = findViewById(R.id.imageView);
+                imageView.setImageDrawable(imageDrawable);
+            }
         }
     }
-}
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_STORAGE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery();
+            } else {
+                Toast.makeText(this, "Permission refusée. Impossible d'accéder à la galerie.", Toast.LENGTH_SHORT).show();
+                // Vous pouvez également afficher une boîte de dialogue personnalisée pour expliquer l'importance de la permission et rediriger l'utilisateur vers les paramètres pour la modifier.
+            }
+        } else if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            } else {
+                Toast.makeText(this, "Permission refusée. Impossible d'accéder à la caméra.", Toast.LENGTH_SHORT).show();
+                // Vous pouvez également afficher une boîte de dialogue personnalisée pour expliquer l'importance de la permission et rediriger l'utilisateur vers les paramètres pour la modifier.
+            }
+        }
+    }
+
+}
